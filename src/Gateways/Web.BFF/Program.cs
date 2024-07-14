84408d6ed9e8ec316d36ffb0aad3e66fd81840f6
Fatih YILDIZ <fthyldz.dev@gmail.com>
2024-07-14 11:24:25 +0300

    add web.bff

---
+++ src/Gateways/Web.BFF/Program.cs
@@ -0,0 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+await app.UseOcelot();
+
+app.Run();
